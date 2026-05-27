4e6445fa73b4b2d525f6231d89d6f1430f6a93f3
Wenn <tangut745@gmail.com>
2026-05-27 16:47:33 +0700

    menambahkan Read Data & Validation

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -3,60 +3,53 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+use App\Models\UserModel; 
 
 class AuthController extends BaseController
 {
+
+    protected $userModel;
+
     function __construct()
     {
         helper('form');
+        $this->userModel = new UserModel();
     }
 
     public function login()
     {
         if ($this->request->getPost()) {
-            $username = $this->request->getVar('username');
-            $password = $this->request->getVar('password');
-
-            $dataUsers = [
-                [
-                    'username' => 'danny',
-                    'password' => '202cb962ac59075b964b07152d234b70', // 123
-                    'role'     => 'admin',
-                    'email'    => 'rdannyoka@dsn.dinus.ac.id'
-                ],
-                [
-                    'username' => 'april',
-                    'password' => '202cb962ac59075b964b07152d234b70', // 123
-                    'role'     => 'admin',
-                    'email'    => 'april@dsn.dinus.ac.id'
-                ],
+            $rules = [
+                'username' => 'required|min_length[6]',
+                'password' => 'required|min_length[7]|numeric',
             ];
 
-            // Cari user yang cocok
-            $foundUser = null;
-            foreach ($dataUsers as $user) {
-                if ($username == $user['username']) {
-                    $foundUser = $user;
-                    break;
-                }
-            }
+            if ($this->validate($rules)) {
+                $username = $this->request->getVar('username');
+                $password = $this->request->getVar('password');
 
-            if ($foundUser) {
-                if (md5($password) == $foundUser['password']) {
-                    session()->set([
-                        'username'   => $foundUser['username'],
-                        'role'       => $foundUser['role'],
-                        'email'      => $foundUser['email'],
-                        'login_time' => date('Y-m-d H:i:s'),
-                        'isLoggedIn' => TRUE
-                    ]);
-                    return redirect()->to(base_url('/'));
+                $dataUser = $this->userModel ->where(['username' => $username])->first();
+                if ($dataUser) {
+	                if (password_verify($password, $dataUser['password'])) {
+                        session()->set([
+                            'username' => $dataUser['username'],
+                            'role' => $dataUser['role'],
+                            'isLoggedIn' => TRUE
+                        ]);
+
+                        return redirect()->to(base_url('/'));
+                    } else {
+                        session()->setFlashdata('failed', 'Username & Password Salah');
+                        return redirect()->back();
+                    }
                 } else {
-                    session()->setFlashdata('failed', 'Username & Password Salah');
+                    session()->setFlashdata('failed', 'Username Tidak Ditemukan');
                     return redirect()->back();
                 }
             } else {
-                session()->setFlashdata('failed', 'Username Tidak Ditemukan');
+                session()->setFlashdata('failed', $this->validator->listErrors());
                 return redirect()->back();
             }
         } else {
