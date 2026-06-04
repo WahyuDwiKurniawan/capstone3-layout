bec4eb6a41945fe83e5b7a954dbac23bdc08751d
Wenn <tangut745@gmail.com>
2026-06-04 21:49:04 +0700

    menambahkan library

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -16,9 +16,25 @@ $routes->group('produk', ['filter' => 'auth'], function ($routes) {
     $routes->post('', 'ProdukController::create');
     $routes->post('edit/(:any)', 'ProdukController::edit/$1');
     $routes->get('delete/(:any)', 'ProdukController::delete/$1');
+    $routes->get('download', 'ProdukController::download');
 });
 
 
-$routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
+$routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
+    //Rute ini digunakan untuk menampilkan isi keranjang belanja
+    $routes->get('', 'TransaksiController::index');
+
+    //Rute ini digunakan untuk menambah produk ke keranjang belanja
+    $routes->post('', 'TransaksiController::cart_add');
+
+    //Rute ini digunakan untuk mengubah jumlah produk pada keranjang belanja
+    $routes->post('edit', 'TransaksiController::cart_edit');
+
+    //Rute ini digunakan untuk menghapus produk dari keranjang belanja
+    $routes->get('delete/(:any)', 'TransaksiController::cart_delete/$1');
+
+    //Rute ini digunakan untuk mengosongkan keranjang belanja
+    $routes->get('clear', 'TransaksiController::cart_clear');
+    });
 
 $routes->get('profile', 'ProfileController::index', ['filter' => 'auth']);
