59393a28fc3672b9a6463e43e2fed7609ecf3502
Wenn <tangut745@gmail.com>
2026-06-16 22:12:35 +0700

    menambahkan Web Service Client

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -36,5 +36,11 @@ $routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
     //Rute ini digunakan untuk mengosongkan keranjang belanja
     $routes->get('clear', 'TransaksiController::cart_clear');
     });
+    
+$routes->get('checkout', 'TransaksiController::checkout', ['filter' => 'auth']);
+$routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
+
+$routes->get('ajax/destinations','TransaksiController::destinations', ['filter' => 'auth']);
+$routes->get('ajax/costs','TransaksiController::costs', ['filter' => 'auth']);
 
 $routes->get('profile', 'ProfileController::index', ['filter' => 'auth']);
