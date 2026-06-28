2f6a07ff9c1418c8cdd00c810c77e71d06dcf00e
Wenn <tangut745@gmail.com>
2026-06-28 14:18:18 +0700

    menambahkan api data produk dan transaksi

--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -24,7 +24,16 @@
                     <span>Produk</span>
                 </a>
             </li><!-- End Produk Nav --> 
-        <?php } ?>
+        <?php
+        }
+        ?>
+
+        <li class="nav-item">
+            <a class="nav-link <?php echo (uri_string() == 'history') ? "" : "collapsed" ?>" href="history">
+                <i class="bi bi-person"></i>
+            <span>History</span>
+            </a>
+        </li><!-- End History Nav -->
 
         <!-- Tambahan Profile -->
         <li class="nav-item">
